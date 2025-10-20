a1108c31e0442cd214e66062c44ab3299b4bbe44
bandilem-git <u24675394@tuks.co.za>
2025-10-20 14:04:10 +0200

    zone.h

---
+++ Zone.h
@@ -0,0 +1,69 @@
+//composite
+#ifndef ZONE_H
+#define ZONE_H
+/**
+ * @file Zone.h
+ * @author Banidle Mnyandu(github: bandilem-git)
+ * @brief 
+ * @version 0.1
+ * @date 2025-10-20
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include "Greenhouse.h"
+#include <vector>
+
+/**
+ * @class Zone
+ * @brief Composite in the Composite pattern that can contain other Greenhouse components
+ */
+class Zone : public Greenhouse {
+private:
+    std::vector<Greenhouse*> children; /**< Container for child components */
+
+public:
+    /** @brief Default constructor */
+    Zone() = default;
+    
+    /** @brief Virtual destructor */
+    virtual ~Zone() override = default;
+
+    /**
+     * @brief Add a child component
+     * @param child Pointer to component to add
+     */
+    void add(Greenhouse* child) override;
+
+    /**
+     * @brief Remove a child component
+     * @param child Pointer to component to remove
+     */
+    void remove(Greenhouse* child) override;
+
+    /**
+     * @brief Get child component at index
+     * @param index Position of child to retrieve
+     * @return Pointer to child component
+     */
+    Greenhouse* getChild(std::size_t index) override;
+
+    /**
+     * @brief Get all children components
+     * @return Vector of child component pointers
+     */
+    const std::vector<Greenhouse*>& getChildren() const;
+
+    /**
+     * @brief Check if this is a composite component
+     * @return true as Zone is a composite
+     */
+    bool isComposite() const override;
+
+    /**
+     * @brief Execute operation on this component and children
+     */
+    void execute() override;
+};
+
+#endif
