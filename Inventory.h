aacf9cba0070066b702f55914fcfb7c70e32903b
bandilem-git <u24675394@tuks.co.za>
2025-10-20 12:14:09 +0200

    completed class definition prototype v1

--- Inventory.h
+++ Inventory.h
@@ -1,13 +1,20 @@
 #ifndef INVENTORY_H
 #define INVENTORY_H
 //singleton class
+#include <vector>
 
 class Inventory{
     private:
-        Inventory(); // default constructor to prevent the use of new keyword4
+        Inventory(); // default constructor to prevent the use of new keyword
         static Inventory* instance; // only instance of said  inventory
+        std::vector<Plant*> plants;
+
     public:
+        ~Inventory();
         static Inventory* getInstance();
+        void addPlant(Plant*);      //this line only works once plants actually exists
+        bool removePlant(Plant*);   //this line only works once plants actually exists
+        Iterator* createIterator(); // Iterator for plants
 };
 
 
