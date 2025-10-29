9b30f9e0e40dc5fc4bc301fce9d471595a1dc6d4
bandilem-git <u24675394@tuks.co.za>
2025-10-29 18:51:34 +0200

    Revert "Plant state group"

--- InStorage.cpp
+++
@@ -1,8 +0,0 @@
-#include "InStorage.h"
-#include "Sold.h"
-#include "Plant.h"
-#include <memory>
-
-void InStorage::onSell(Plant& p) {
-    p.changeStatus(std::make_unique<Sold>()); // legal: InStorage -> Sold
-}
