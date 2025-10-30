2e61f882b9613a1800515d6479ecbbe0bf4eb7fc
HBPencil28 <u24868346@tuks.co.za>
2025-10-30 21:06:53 +0200

    Delete Command and files for testing

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "CareCommand.h"
+#include "Staff.h"
+#include "StaffFactory.h"
+#include "StaffHandler.h"
+#include "Plant.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main{
+    
+}
