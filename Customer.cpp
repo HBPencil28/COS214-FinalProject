40505567593e08458fccaec830c5b1afb2892ad0
Njabs-codez <njabsmathonsi52@gmail.com>
2025-10-31 01:31:44 +0200

    Mostly implemented customer.

---
+++ Customer.cpp
@@ -0,0 +1,28 @@
+#include "Customer.h"
+
+Customer::~Customer(){
+    for(BasePlant* b : purchases){
+        if(b)
+            delete b;
+    }
+}
+
+void Customer::buyPlant(Staff* attender, Order* purchaseDetails){
+    CustomerCommand* c = new BuyPlant(*purchaseDetails, attender, this);
+    c->execute();
+}
+
+void Customer::requestHelp(Staff* attender, std::string& question){
+    CustomerCommand* c = new RequestHelp(question, attender);
+    c->execute();
+}
+
+void Customer::customiseMyPlant(Staff* attender, std::string& accessory){
+    BasePlant* p = purchases.at(0);
+    CustomerCommand* c = new CustomisePlant(accessory, attender, p, this);
+    c->execute();
+}
+
+void Customer::addPurchases(BasePlant* p){
+    purchases.push_back(p);
+}
